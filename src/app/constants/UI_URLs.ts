7d6cc0929fb36833127aecc927c21d68825f851a
Mohamed Goher <goherm200@gmail.com>
2020-07-19 23:58:20 +0200

    Add ui_urls and backend_urls costants.

---
+++ src/app/constants/UI_URLs.ts
@@ -0,0 +1,7 @@
+export const UIPagesUrls = {
+    HOME_PAGE_URL : "home",
+    REGISTERATION_PAGE_URL : "registration",
+    LOGIN_PAGE_URL : "login",
+    PROFILE_PAGE_URL:"profile"
+    
+};
---
+++ src/app/constants/urls.ts
@@ -0,0 +1,9 @@
+authentication:{
+    LOGIN_URL : "";
+    REGISTERATION_URL : "";
+};
+
+UserEnitiyURLS:{
+    ADD_USER_URL:"";
+    DELETE_USER_URL:"";
+}
