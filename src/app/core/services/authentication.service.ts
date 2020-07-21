e23fea94538c9d4f177ec6ba919bfbf2fb1fc3e0
Mohamed Goher <goherm200@gmail.com>
2020-07-21 21:46:21 +0200

    Add authentication method logic.

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Authentication } from 'src/app/constants/BackEnd_URLS';
+import { AuthenticationRequest } from '../models/AuthenticationRequest.model';
+import { Observable } from 'rxjs';
+
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthenticationService {
+  private _obs:Observable<Object>;
+
+
   login(username: any, password: any):String {
     // send to backend
     throw new Error("Method not implemented.");
@@ -13,6 +21,11 @@ export class AuthenticationService {
     return true;
   }
 
-  constructor()
-   { }
+  constructor(private http:HttpClient ){ }
+
+  // function definition.
+   authenticate(_authenticationRequest: AuthenticationRequest):Observable<Object>{
+    this._obs = this.http.post(Authentication.AUTHENTICATE_URL,_authenticationRequest);
+    return this._obs;
+  }
 }
