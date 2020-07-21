0776c1e7c2ea3f6bf616dabeb5b8522ef0074a84
Mohamed Goher <goherm200@gmail.com>
2020-07-21 21:43:15 +0200

    Update back end urls.

---
+++ src/app/constants/BackEnd_URLS.ts
@@ -0,0 +1,9 @@
+export const  Authentication={
+    REGISTERATION_URL : "",
+    AUTHENTICATE_URL:"localhost:8080/authenticate"
+};
+
+export const  UserEnitiyURLS={
+    ADD_USER_URL:"",
+    DELETE_USER_URL:""
+}
--- src/app/constants/urls.ts
+++
@@ -1,9 +0,0 @@
-authentication:{
-    LOGIN_URL : "";
-    REGISTERATION_URL : "";
-};
-
-UserEnitiyURLS:{
-    ADD_USER_URL:"";
-    DELETE_USER_URL:"";
-}
